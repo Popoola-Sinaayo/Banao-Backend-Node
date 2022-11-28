341f1afc4d94443e6cc92415c69e16f8b67c6a5c
Popoola-Sinaayo <olusegunpopoola4real@gmail.com>
2022-11-29 00:09:14 +0100

    updated index.js

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const routes = require("./routes/route");
 const app = express();
 app.use(bodyParser.json());
 app.use(routes);
-mongoose
-  .connect(
-    "mongodb+srv://Popoola:prayer1020@cluster0.mbbmo7d.mongodb.net/banao?retryWrites=true&w=majority"
-  )
-  .then((data) => {
-    console.log("Connected");
-    app.listen("5000");
-  })
-  .catch((err) => {
-    console.log("Got an error", err);
-  });
+mongoose.connect(
+  "mongodb+srv://Popoola:prayer1020@cluster0.mbbmo7d.mongodb.net/banao?retryWrites=true&w=majority"
+);
+
+module.exports = app
+// .then((data) => {
+//   console.log("Connected");
+//   app.listen("5000");
+// })
+// .catch((err) => {
+//   console.log("Got an error", err);
+// });
